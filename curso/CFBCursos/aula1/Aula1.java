6a235e553c1269edf38be21a420dea0ac4ff066e
Dante <layscontato@gmail.com>
2021-09-29 16:24:28 -0300

    aula 1 e 2

---
+++ curso/CFBCursos/aula1/Aula1.java
@@ -0,0 +1,11 @@
+public class Aula1 {
+  public static void main(String[] args) {
+    // print não tem quebra de linha
+    System.out.print("O começo");
+    // System.out.print("O começo\n"); tem quebra de linha no final
+    // println tem quebra de linha no final
+    System.out.println("Aula1 e 2");
+    // %s string e %d inteiro
+    System.out.printf("%s - %d", "Setembro", 2021);
+  }
+}

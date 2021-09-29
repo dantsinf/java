84a8879b567112b78b414fc083e47b90d16cb567
Dante <layscontato@gmail.com>
2021-09-29 17:19:57 -0300

    aula 3

---
+++ curso/CFBCursos/aula3/Aula3.java
@@ -0,0 +1,47 @@
+public class Aula3 {
+  public static void main(String[] args) {
+    // IF
+    // IF ELSE
+    // Switch
+
+    int nota = 90;
+    int faltas = 10;
+    int maxFaltas = 5;
+    int media = 60;
+    String res;
+
+    // part1 ifelse
+    /*
+     * if (nota >= media) { System.out.println("Aprovado"); } else {
+     * System.out.println("Reprovado"); } System.out.println("Final do programa");
+     * // System.out.println("Aula1 e 2");
+     */
+
+    // part2 e &&
+    /*
+     * if (nota >= media && faltas <= maxFaltas) { System.out.println("Aprovado"); }
+     * else { System.out.println("Reprovado"); }
+     */
+
+    // part3 IF Ternário
+    /*
+     * res = (nota >= media ? "Aprovado" : "Reprovado");
+     * System.out.println("Resultado: " + res);
+     */
+
+    // Switch
+    int pos = 1;
+    switch (pos) {
+      case 1:
+        System.out.println("Primeiro lugar");
+        break;
+      case 2:
+        System.out.println("Segundo lugar");
+        break;
+      default:
+        System.out.println("Não subiu no pódio");
+        break;
+    }
+
+  }
+}
---
+++ curso/CFBCursos/aula3/README.md
@@ -0,0 +1,3 @@
+# Aula 3
+
+### IF, IF ELSE, Switch

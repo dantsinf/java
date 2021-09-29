7dfef0a152bd89cdcb8fd4a14faf49ae4689c3ba
Dante <layscontato@gmail.com>
2021-09-29 16:35:18 -0300

    final aula 1 e 2

--- curso/CFBCursos/aula1/Aula1.java
+++ curso/CFBCursos/aula1/Aula1.java
@@ -1,5 +1,9 @@
 public class Aula1 {
   public static void main(String[] args) {
+
+    int num = 100;
+    String nome = "Maria";
+
     // print não tem quebra de linha
     System.out.print("O começo");
     // System.out.print("O começo\n"); tem quebra de linha no final
@@ -7,5 +11,7 @@ public class Aula1 {
     System.out.println("Aula1 e 2");
     // %s string e %d inteiro
     System.out.printf("%s - %d", "Setembro", 2021);
+    // %n é quebra de linha
+    System.out.printf("\nValor da variável num:%d%nNome:%s", num, nome);
   }
 }

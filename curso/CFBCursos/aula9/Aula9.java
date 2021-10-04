f86c640dfdb2da1257d1e37f8b0d79554e82203f
Dante <layscontato@gmail.com>
2021-10-04 14:02:06 -0300

    aula 09

--- curso/CFBCursos/aula9/Aula9.java
+++ curso/CFBCursos/aula9/Aula9.java
@@ -20,14 +20,42 @@ public class Aula9 {
      */
 
     // part3
+    /*
+     * int r;
+     * 
+     * msg("Java", 1); r = soma(10, 5); System.out.println(r); }
+     * 
+     * public static int soma(int n1, int n2) { int res = n1 + n2; return res; }
+     * 
+     * public static void msg(String m, int l) { for (int i = 0; i < l; i++) {
+     * System.out.println(m); } }
+     */
+
     int r;
 
     msg("Java", 1);
-    r = soma(10, 5);
-    System.out.println(r);
+    System.out.println(soma2(10, 20));
+    System.out.println(soma(2, 5));
+    System.out.println(soma(2.5, 4.3));
   }
 
-  public static int soma(int n1, int n2) {
+  public static int soma(int... numeros) {
+    int res = 0;
+    for (int n : numeros) {
+      res += n;
+    }
+    return res;
+  }
+
+  public static Double soma(Double... numeros) {
+    Double res = 0.0;
+    for (Double n : numeros) {
+      res += n;
+    }
+    return res;
+  }
+
+  public static int soma2(int n1, int n2) {
     int res = n1 + n2;
     return res;
   }
@@ -37,5 +65,4 @@ public class Aula9 {
       System.out.println(m);
     }
   }
-
 }

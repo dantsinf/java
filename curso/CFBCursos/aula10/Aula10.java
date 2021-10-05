dfb68c90e901a77f1399e7b7af3f935d550eb1e8
Dante <layscontato@gmail.com>
2021-10-05 15:33:46 -0300

    aula 10

--- curso/CFBCursos/aula10/Aula10.java
+++ curso/CFBCursos/aula10/Aula10.java
@@ -2,7 +2,16 @@ import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class Aula10 {
-  public static void main(String[] args) {
+  // part1
+  // public static void main(String[] args) {
+  // Jogador j1 = new Jogador();
+  // Jogador j2 = new Jogador();
+  // Jogador j3 = new Jogador();
+  // }
 
+  public static void main(String[] args) {
+    Jogador j1 = new Jogador(1);
+    Jogador j2 = new Jogador(2);
+    Jogador j3 = new Jogador(3);
   }
 }
--- curso/CFBCursos/aula10/Jogador.java
+++ curso/CFBCursos/aula10/Jogador.java
@@ -1,3 +1,10 @@
 public class Jogador {
+  // part1
+  // public Jogador() {
+  // System.out.printf("%nJogador criado%n");
+  // }
 
+  public Jogador(int n) {
+    System.out.printf("%nJogador numero %d criado%n", n);
+  }
 }

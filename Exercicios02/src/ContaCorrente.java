80897bb5681d996a69d58393047e797b1ceffd9f
Dante <layscontato@gmail.com>
2021-12-16 10:52:05 -0300

    mudancas na estrutura das pastas

---
+++ Exercicios02/src/ContaCorrente.java
@@ -0,0 +1,30 @@
+public class ContaCorrente {
+
+    private int conta;
+    private Double saldo;
+
+    public static void main(String[] args) {
+    }
+
+    public ContaCorrente(Double saldo){
+        this.saldo = saldo;
+    }
+
+    public void deposito(double valor){
+
+    }
+
+    public double retirada(double valor){
+    return valor;
+    }
+
+    public double getSaldo(){
+    return saldo;
+    }
+
+    public int getNroConta(){
+    return conta;
+    }
+
+
+}//fecha Classe
---
+++ Exercicios02/src/Excercicio02Data08112021/Exercicio02data08112021.java
@@ -0,0 +1,10 @@
+package Excercicio02Data08112021;
+
+import java.util.ArrayList;
+
+public class Exercicio02data08112021 {
+    public static void main(String[] args) {
+        List lista = new ArrayList();
+    }
+    public static 
+}
